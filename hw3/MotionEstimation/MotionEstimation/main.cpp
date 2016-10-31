6ab2513e88471320e31e91274130bde964ead38d
Azure <cvbnm0002@gmail.com>
2016-10-31 17:42:07 +0800

    Read *.Y is done

--- hw3/MotionEstimation/MotionEstimation/main.cpp
+++ hw3/MotionEstimation/MotionEstimation/main.cpp
@@ -4,12 +4,31 @@
 #include <opencv2\highgui\highgui.hpp>
 #include <opencv2\imgproc\imgproc.hpp>
 
+using namespace std;
 using namespace cv;
+
+Mat ReadFile(string fileName)
+{
+	char data;
+	int len = 0;
+	Mat result(Size(720, 480), CV_8UC1);
+	FILE *file = fopen(fileName.c_str(), "rb");
+	while ((data = fgetc(file)) != EOF)
+	{
+		result.data[len++] = (unsigned)data;
+	}
+	fclose(file);
+	return result;
+}
 
 int main()
 {
-	//Mat mat = imread("Images\\mobile0.Y");
-	//imshow("", mat);
-	//waitKey();
+	Mat m0 = ReadFile("Images\\mobile0.Y");
+	Mat m1 = ReadFile("Images\\mobile1.Y");
+	Mat m2 = ReadFile("Images\\mobile2.Y");
+	imshow("m0.bmp", m0);
+	imshow("m1.bmp", m1);
+	imshow("m2.bmp", m2);
+	waitKey();
 	return 0;
 }
