a11aa09e318d5629f59a52bc94bfd433e403d095
Azure <cvbnm0002@gmail.com>
2016-10-31 12:56:12 +0800

    Establish motion estimation of hw3

---
+++ hw3/MotionEstimation/MotionEstimation/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <opencv2\opencv.hpp>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+using namespace cv;
+
+int main()
+{
+	//Mat mat = imread("Images\\mobile0.Y");
+	//imshow("", mat);
+	//waitKey();
+	return 0;
+}
